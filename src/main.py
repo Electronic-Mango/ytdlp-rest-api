c55a01279c0caad625cffea4afd364e13f199322
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-09-18 20:03:59 +0200

    Make cleanup and selecting downloaded file more robust
    Select the downloaded file through a yt-dlp hook, rather than search for the file manually.
    Use downloaded metadata to redirect to thumbnail, rather than run a separate download.
    Download the file to a temporary directory and remove entire directory after sending. This way any "partial" files should also be removed.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from os import getenv
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import Any
 from uuid import uuid4
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Response
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, HTTPException, Response
+from fastapi.responses import FileResponse, RedirectResponse
 from loguru import logger
 from starlette.background import BackgroundTask
 from yt_dlp import YoutubeDL, match_filter_func
@@ -15,9 +16,6 @@ load_dotenv()
 DEFAULT_MAX_DURATION = getenv("MAX_DURATION")
 DEFAULT_FORMAT = getenv("FORMAT")
 DEFAULT_FORMAT_SORT = getenv("FORMAT_SORT")
-
-ENCODING = "UTF-8"
-THUMBNAIL_PARAMS = {"writethumbnail": True, "skip_download": True}
 
 app = FastAPI()
 
@@ -30,13 +28,31 @@ def download(
     format_sort: str | None = DEFAULT_FORMAT_SORT,
 ) -> Response:
     logger.info(f"[{video_url}] Received request")
-    filename = str(uuid4())
-    params = video_params(max_duration, format, format_sort)
-    file = download_file(video_url, filename, params)
-    if not file:
-        logger.info(f"[{video_url}] Falling back to thumbnail")
-        file = download_file(video_url, filename, THUMBNAIL_PARAMS)
-    return FileResponse(file, background=BackgroundTask(file.unlink))
+    request_id = str(uuid4())
+    target_dir = TemporaryDirectory(prefix=request_id)
+    target_path = Path(target_dir.name) / request_id
+    try:
+        params = video_params(max_duration, format, format_sort)
+        result = download_file(video_url, target_path, params)
+        return prepare_response(video_url, result, target_dir)
+    except Exception:
+        target_dir.cleanup()
+        raise
+
+
+def prepare_response(
+    video_url: str,
+    result: Path | str | None,
+    target_dir: TemporaryDirectory,
+) -> Response:
+    if not result:
+        raise HTTPException(status_code=502, detail="No media found")
+    if isinstance(result, Path):
+        logger.info(f"[{video_url}] Sending file response [{result}]")
+        return FileResponse(result, background=BackgroundTask(target_dir.cleanup))
+    logger.info(f"[{video_url}] Redirecting to thumbnail [{result}]")
+    target_dir.cleanup()
+    return RedirectResponse(url=result) if result else None
 
 
 def video_params(
@@ -54,11 +70,20 @@ def video_params(
     return params
 
 
-def download_file(video_url: str, filename: str, params: dict[str, Any]) -> Path | None:
-    with YoutubeDL({"outtmpl": f"{filename}.%(ext)s"} | params) as ytdl:
-        ytdl.download(video_url)
-    return find_downloaded_file(filename)
+def download_file(
+    video_url: str, target: Path, params: dict[str, Any]
+) -> Path | str | None:
+    result: Path | None = None
 
+    def capture_path(path: str) -> None:
+        nonlocal result
+        result = Path(path)
 
-def find_downloaded_file(name: str) -> Path | None:
-    return next((f for f in Path().iterdir() if f.is_file() and f.stem == name), None)
+    final_params = {
+        "outtmpl": f"{target}.%(ext)s",
+        "post_hooks": [capture_path],
+        **params,
+    }
+    with YoutubeDL(final_params) as ytdl:
+        info = ytdl.extract_info(video_url, download=True)
+        return result if result and result.is_file() else info.get("thumbnail")
